472aba74df6411e094471c6dc4d34f6e8c0b21b0
Nikolas <nikolastroupkos@gmail.com>
2025-08-21 09:17:24 -0300

    Se agrega vista register

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,21 +3,39 @@ import Logo from "./Logo";
 import Button from "./Button";
 import style from "./Button.module.css";
 import headerStyle from "./Header.module.css";
-import { useRouter } from "next/navigation";
-
-
+import { useRouter, usePathname } from "next/navigation";
 export default function Header() {
   const router = useRouter();
+  const pathname = usePathname();
+  const routeClass =
+    pathname === "/login" ? headerStyle.routeLogin : headerStyle.routeHome;
+
   const goToLogin = () => router.push("/login");
+  const goToRegister = () => router.push("/register");
+
   return (
-    <header className={headerStyle.header}>
+    <header className={`${headerStyle.header} ${routeClass}`}>
       <div>
         <Logo onClick={() => router.push("/")} />
       </div>
-      
+
       <div className={style.buttonGroup}>
-        <Button className={style.button1} label="Ingresar" backgroundColor="var(--dark)" border="solid var(--lima) 1px" textColor="var(--lima)" onClick={goToLogin} />
-        <Button className={style.button2} label="Crear cuenta" backgroundColor="var(--lima)" border="" textColor="black" />
+        <Button
+          className={style.button1}
+          label="Ingresar"
+          backgroundColor="var(--dark)"
+          border="solid var(--lima) 1px"
+          textColor="var(--lima)"
+          onClick={goToLogin}
+        />
+        <Button
+          className={style.button2}
+          label="Crear cuenta"
+          backgroundColor="var(--lima)"
+          border=""
+          textColor="black"
+          onClick={goToRegister}
+        />
       </div>
     </header>
   );
---
+++ src/app/register/page.tsx
@@ -0,0 +1,8 @@
+export default function register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      {/* Registration form goes here */}
+    </div>
+  );
+}
